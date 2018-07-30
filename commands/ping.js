a14d8880fdd21493c4f64cea65a75d567881bd21
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-07-30 19:07:05 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send(`${Date.now() - message.createdTimestamp} Pong!`)
+    message.channel.send(`Pong! ${Date.now() - message.createdTimestamp}`)
 }
